aec06f5f6f2501f5168c8ac1fbd1b51a324d1940
Alexander Shchepetilnikov <alex@irqed.com>
2013-11-16 16:21:45 +0400

    __init__.py who doesn't like star wars

--- octokit/__init__.py
+++ octokit/__init__.py
@@ -1,2 +1,29 @@
-__version_info__ = ('0', '0', '1')
-__version__ = '.'.join(__version_info__)
+# encoding: utf-8
+
+#                           /~\   Hey you, wanna help?
+#                          (O O) _/
+#                          _\=/_
+#          ___            /  _  \
+#         / ()\          //|/.\|\\
+#       _|_____|_       ||  \_/  ||
+#      | | === | |      || |\ /| ||
+#      |_|  O  |_|       # \_ _/ #
+#       ||  O  ||          | | |
+#       ||__*__||          | | |
+#      |~ \___/ ~|         []|[]
+#      /=\ /=\ /=\         | | 
+#______[_]_[_]_[_]________/_]_[_\_____
+
+"""
+Octokit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+Octokit is a toolkit for the GitHub API.
+
+:copyright: (c) 2013 by Alexander Shchepetilnikov.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Alexander Shchepetilnikov'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Alexander Shchepetilnikov'
