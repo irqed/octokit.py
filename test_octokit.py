c64a33b9f7f911baa43230b67c0b6bd9d58a83fb
Alexander Shchepetilnikov <alex@irqed.com>
2013-11-18 15:53:23 +0400

    Base user test case added.

--- test_octokit.py
+++ test_octokit.py
@@ -13,33 +13,56 @@ class AuthenticationTestCase(unittest.TestCase):
 
     _multiprocess_can_split_ = True
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+    def test_unauthenticated(self):
+        hub = octokit.Octokit()
+        assert hub.authenticated is False
 
     def test_basic(self):
         hub = octokit.Octokit(login='', password='')
+        assert hub.authenticated is True
         assert hub.basic_authenticated is True
 
     def test_token(self):
         hub = octokit.Octokit(access_token='', client_secret='')
         assert hub.basic_authenticated is False
+
+        assert hub.authenticated is True
         assert hub.token_authenticated is True
 
     def test_application(self):
         hub = octokit.Octokit(client_id='', client_secret='')
         assert hub.basic_authenticated is False
         assert hub.token_authenticated is False
+
+        assert hub.authenticated is True
         assert hub.application_authenticated is True
 
     def test_user(self):
         hub = octokit.Octokit(login='', password='')
         hub_two = octokit.Octokit(access_token='', client_secret='')
-
         assert hub.application_authenticated is False
         assert hub_two.application_authenticated is False
 
         assert hub.user_authenticated is True
         assert hub_two.user_authenticated is True
+
+        assert hub.authenticated is True
+        assert hub_two.authenticated is True
+
+
+class UserTestCase(unittest.TestCase):
+
+    _multiprocess_can_split_ = True
+
+    def test_user(self):
+        user = octokit.Octokit(login='', password='').user
+        assert user.name == ''
+
+    def test_another_user(self):
+        hub = octokit.Octokit(login='', password='')
+        assert hub.users['username'].name == ''
+
+    def test_users(self):
+        users = octokit.Octokit(login='', password='').users
+        assert len(users) == 100
+        assert len(users.next()) == 100  # get the second page of the results
