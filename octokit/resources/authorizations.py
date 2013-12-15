0135a8aad4b4b2e6bd0ac6150669b0024b2cf143
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-15 22:59:44 +0400

    Edit doctring

--- octokit/resources/authorizations.py
+++ octokit/resources/authorizations.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 """Methods for the Authorizations API
+http://developer.github.com/v3/oauth/#oauth-authorizations-api
 """
 
 from octokit.resources.base import Resource
@@ -8,7 +9,6 @@ from octokit.resources.base import Resource
 
 class Authorizations(Resource):
     """Authorizations API resource
-    http://developer.github.com/v3/oauth/#oauth-authorizations-api
     """
     url = '/authorizations'
 
