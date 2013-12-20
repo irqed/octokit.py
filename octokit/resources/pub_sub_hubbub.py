fdfe6cef68878c0428e5f19ed8c6fe87135419f0
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-20 17:16:52 +0400

    PubSubHubbub resource with not implemented methods

--- octokit/resources/pub_sub_hubbub.py
+++ octokit/resources/pub_sub_hubbub.py
@@ -3,3 +3,42 @@
 """Methods for the PubSubHubbub API
 http://developer.github.com/v3/repos/hooks/#pubsubhubbub
 """
+
+from octokit.resources.base import Resource
+
+
+class PubSubHubbub(Resource):
+    """PubSubHubbub API resource
+    """
+    url = None
+
+    def __init__(self, **kwargs):
+        super(PubSubHubbub, self).__init__(**kwargs)
+
+    def subscribe(self):
+        """Subscribe to a pubsub topic
+
+        http://developer.github.com/v3/repos/hooks/#subscribing
+        """
+        raise NotImplementedError
+
+    def unsubscribe(self):
+        """Unsubscribe from a pubsub topic
+
+        http://developer.github.com/v3/repos/hooks/#subscribing
+        """
+        raise NotImplementedError
+
+    def subscribe_service_hook(self):
+        """Subscribe to a repository through pubsub
+
+        http://developer.github.com/v3/repos/hooks/#subscribing
+        """
+        raise NotImplementedError
+
+    def unsubscribe_service_hook(self):
+        """Unsubscribe repository through pubsub
+
+        http://developer.github.com/v3/repos/hooks/#subscribing
+        """
+        raise NotImplementedError
