74c4f874d9450ab71ffbccd87722fbab1b3a5c85
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-20 17:35:47 +0400

    RateLimit resource with not implemented methods

--- octokit/resources/rate_limit.py
+++ octokit/resources/rate_limit.py
@@ -3,3 +3,22 @@
 """Methods for API rate limiting info
 http://developer.github.com/v3/#rate-limiting
 """
+
+from octokit.resources.base import Resource
+
+
+class RateLimit(Resource):
+    """RateLimit API resource
+    """
+    url = None
+
+    def __init__(self, **kwargs):
+        super(RateLimit, self).__init__(**kwargs)
+
+    def rate_limit(self):
+        """Refresh rate limit info by making a new request
+
+        http://developer.github.com/v3/rate_limit/#rate-limit
+        """
+
+        raise NotImplementedError
