93a6650d79490173a4309d0a997d2db83f0e379a
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-20 14:29:08 +0400

    Hooks resource with not implemented methods

--- octokit/resources/hooks.py
+++ octokit/resources/hooks.py
@@ -3,3 +3,20 @@
 """Methods for the Hooks API
 http://developer.github.com/v3/repos/hooks/
 """
+
+from octokit.resources.base import Resource
+
+
+class Hooks(Resource):
+    """Hooks API resource
+    """
+    url = '/hooks'
+
+    def __init__(self, **kwargs):
+        super(Hooks, self).__init__(**kwargs)
+
+    def available_hooks(self):
+        """List all Service Hooks supported by GitHub
+        http://developer.github.com/v3/repos/hooks/#services
+        """
+        raise NotImplementedError
