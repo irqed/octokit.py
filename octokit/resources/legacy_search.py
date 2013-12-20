7f0aea7549f4113d77fb5fb5de5f8c3fc9328cb1
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-20 15:53:10 +0400

    LegacySearch resource with not implemented methods

--- octokit/resources/legacy_search.py
+++ octokit/resources/legacy_search.py
@@ -3,3 +3,31 @@
 """Methods for the Legacy Search API
 http://developer.github.com/v3/search/
 """
+
+from octokit.resources.base import Resource
+
+
+class LegacySearch(Resource):
+    """LegacySearch API resource
+    """
+    url = None
+
+    def __init__(self, **kwargs):
+        super(LegacySearch, self).__init__(**kwargs)
+
+    def legacy_search_repositories(self):
+        """Legacy repository search
+        http://developer.github.com/v3/search/#search-repositories
+        """
+        raise NotImplementedError
+
+    def legacy_search_issues(self):
+        """Legacy search issues within a repository
+        """
+        raise NotImplementedError
+
+    def legacy_search_users(self):
+        """Search for user
+        http://developer.github.com/v3/search/#search-users
+        """
+        raise NotImplementedError
