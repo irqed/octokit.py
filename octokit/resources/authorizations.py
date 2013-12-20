01fad8740aae120a17255c224c62558a4eeeb193
Alexander Shchepetilnikov <alex@irqed.com>
2013-12-20 13:33:27 +0400

    Authorizations resource with not implemented methods

--- octokit/resources/authorizations.py
+++ octokit/resources/authorizations.py
@@ -21,3 +21,35 @@ class Authorizations(Resource):
             return self._http.get(self.url + "/%s" % auth_id)
         else:
             return self._http.get(self.url)
+
+    def create_authorization(self):
+        """You can create your own tokens, and only through
+        http://developer.github.com/v3/oauth/#create-a-new-authorization
+        http://developer.github.com/v3/oauth/#scopes
+        """
+        raise NotImplementedError
+
+    def update_authorization(self):
+        """Update an authorization for the authenticated user.
+        http://developer.github.com/v3/oauth/#update-a-new-authorization
+        http://developer.github.com/v3/oauth/#scopes
+        """
+        raise NotImplementedError
+
+    def delete_authorization(self):
+        """Delete an authorization for the authenticated user
+        http://developer.github.com/v3/oauth/#delete-an-authorization
+        """
+        raise NotImplementedError
+
+    def scopes(self):
+        """Check scopes for a token
+        http://developer.github.com/v3/oauth/#scopes
+        """
+        raise NotImplementedError
+
+    def authorize_url(self):
+        """Get the URL to authorize a user for an application via the web flow
+        http://developer.github.com/v3/oauth/#web-application-flow
+        """
+        raise NotImplementedError
